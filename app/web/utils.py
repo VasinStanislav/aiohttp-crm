66ea2e990990bed5d9cb7471b7032e4ccd2d0639
VasinStanislav <flow98.130@mail.ru>
2022-08-18 17:49:18 +0300

    error handling middleware was added

--- app/web/utils.py
+++ app/web/utils.py
@@ -1,6 +1,5 @@
-from typing import Any
-from aiohttp.web_response import Response
-from aiohttp.web_response import json_response as aiohttp_json_response
+from typing import Any, Optional
+from aiohttp.web_response import Response, json_response as aiohttp_json_response
 
 
 def json_response(data: Any=None, status: str='ok') -> Response:
@@ -8,4 +7,13 @@ def json_response(data: Any=None, status: str='ok') -> Response:
         data = {}
     return aiohttp_json_response(data={
         'status': status, 'data': data
+        })
+
+
+def error_json_response(http_status: int, status: str='error', message: Optional[str]=None, 
+                        data: Optional[dict]=None):
+    if data is None:
+        data = {}
+    return aiohttp_json_response(status=http_status, data={
+        'status': status, 'data': data, 'message': str(message)
         })
