ac3bf7f2b5ccb10f9caf8d45914b29ce7602eaac
VasinStanislav <flow98.130@mail.ru>
2022-08-18 14:02:07 +0300

    new methods for business logic (no validation)

--- app/store/crm/accessor.py
+++ app/store/crm/accessor.py
@@ -1,5 +1,6 @@
 from typing import Optional
 import typing
+import uuid
 
 
 if typing.TYPE_CHECKING:
@@ -27,3 +28,12 @@ class CrmAccessor:
 
     async def add_user(self, user: User):
         self.app.database['users'].append(user)
+
+    async def list_users(self) -> list[User]:
+        return self.app.database['users']
+
+    async def get_user(self, user_id: uuid.UUID) -> Optional[User]:
+        for user in self.app.database['users']:
+            if user.id_ == user_id:
+                return user
+        return None
